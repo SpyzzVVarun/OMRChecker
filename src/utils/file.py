e23d053c8d791da60bf96d032ea3561a46eeddd8
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2022-10-03 10:51:38 +0530

    Updated to add code for schema validation

--- src/utils/file.py
+++ src/utils/file.py
@@ -1,7 +1,28 @@
 import json
-
+import jsonschema
+from jsonschema import validate
 
 def load_json(path, **rest):
     with open(path, "r") as f:
         loaded = json.load(f, **rest)
     return loaded
+
+def load_schema(path,**rest):
+    with open(path, 'r') as f:
+        schema = json.load(f,**rest)
+    return schema
+
+
+def validate_json(json_data,schema_path):
+    execute_api_schema = load_schema(schema_path)
+
+    try:
+        validate(instance=json_data, schema=execute_api_schema)
+
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        err = "Given JSON data is Invalid"
+        return False, err
+
+    message = "Given JSON data is Valid"
+    return True, message
